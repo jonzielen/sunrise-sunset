31b907a0d61d9dc7a3499681501a3e89d6cdd677
Jon Zielenkievicz <jonzielen@gmail.com>
2015-09-19 14:04:33 -0400

    first commit

---
+++ classes/sunrisesunset.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Sunrisesunsetjz;
+
+class Sunrisesunset {
+
+    __construct() {
+
+    }
+
+    protected function addMessage() {
+        return 'this is the message';
+    }
+
+    protected function
+
+
+
+}
