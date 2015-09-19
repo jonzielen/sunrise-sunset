e8f1f3f52438bbe2c0d53c3190bb9b32c63b7e03
Jon Zielenkievicz <jonzielen@gmail.com>
2015-09-19 14:55:19 -0400

    adds function to format phonenumbers

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php  ?>
+<?php
+    $phoneNumbers[] = [
+        'phoneNumber' => '9082087183',
+        'provider' => 'verizon'
+    ];
+
+    $textProvider['verizon'] = '@vtext.com';
+
+    function buildTextArray($phoneNumbers, $textProvider) {
+        $fullTextAddres = [];
+        foreach ($phoneNumbers as $person) {
+            $fullTextAddres[] = $person['phoneNumber'].$textProvider[$person['provider']];
+
+        }
+        return $fullTextAddres;
+    }
+
+
+    echo '<pre>';
+    print_r(buildTextArray($phoneNumbers, $textProvider));
+    echo '</pre>';
+?>
